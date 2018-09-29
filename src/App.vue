45482772bdcc498c3ee464ebb8eaab7bb9c92af7
Parag Gawde <paraggawde.303@gmail.com>
2018-09-29 15:49:31 +0530

    Getting proper results with ratings

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,25 @@
 <template>
-  <div id="App">
+  <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>Movie Revue App</h1>
     <h3>The best place to get Movie Reviews</h3>
-    <Movie v-on:formSubmit="getMovieList"/>
-    <br>
-    <List v-text="movie"/>
+    <Movie/>
   </div>
 </template>
 
 <script>
 import Movie from "./components/Movie.vue";
-import List from "./components/List.vue";
-import axios from "axios";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    Movie,
-    List
-  },
-  data: function() {
-    return {
-      inputTitle: "",
-      movie: ""
-    };
-  },
-  methods: {
-    getMovieList: function(inputTitle) {
-      this.axios
-        .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=7b83dfba4032b1b1d48a4e350ad6b79d&query=" +
-            inputTitle
-        )
-        .then((response) => {
-          var ListLength = response.data.results.length;
-          // for (var i = 0; i < ListLength; i++) {
-          //   this.movieTitle = response.data.results[i].title +
-          //     response.data.results[i].vote_average;
-          // }
-          this.movie = response.data.results[0].title +": "+ response.data.results[0].vote_average + " " + response.data.results[0].overview
-        });
-    }
+    Movie
   }
 };
 </script>
 
 <style>
-#App {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+#app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,26 +1,44 @@
 <template>
     <div id="Movie">
-        <form @submit.prevent='formSubmit'>
-            <input type="text" v-model="movieTitle">
+        <form v-on:submit.prevent='getMovieList(inputTitle)'>
+            <input type="text" v-model="inputTitle">
             <input type="submit" value="Submit">
         </form>
+        <div id="movieDisplay" v-for="movie in movies">
+            <p>{{movie.title}}</p>
+            <p>{{movie.vote_average}}</p>
+            <!-- <p>{{movieDescription}}</p> -->
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'Movie',
-    data: function(){
-        return{
-            movieTitle: ''
-        }
-    },
-    methods: {
-        formSubmit(e){
-            this.$emit('formSubmit', this.movieTitle);
-            e.preventDefault();
-        }
+  name: "Movie",
+  data: function() {
+    return {
+      inputTitle: "",
+      movies:"",
+    //   movieTitle: "",
+    //   movieRating: "",
+    //   movieDescription: ""
+    };
+  },
+  methods: {
+    getMovieList: function(inputTitle) {
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=7b83dfba4032b1b1d48a4e350ad6b79d&query=" +
+            inputTitle
+        )
+        .then((response) => {
+            this.movies = response.data.results;
+        //   this.movieTitle = response.data.results[0].title;
+        //   this.movieRating = response.data.results[0].vote_average;
+        //   this.movieDescription = response.data.results[0].overview
+        });
     }
-}
+  }
+};
 </script>
 
